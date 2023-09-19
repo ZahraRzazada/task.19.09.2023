9c2cf47bdb434b81edd78e0135e76eb7e91a7c09
ZahraRzazada <zahraer@code.edu.az>
2023-09-19 09:35:29 +0400

    Added

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let btn =document.getElementById('btn')
+btn.onclick=function(){
+    let count =parseInt(document.getElementById('count').value)
+    let percent =parseInt(document.getElementById('percent').value)
+    let time =parseInt(document.getElementById('time').value)
+    mebleg.innerHTML=count
+    faiz.innerHTML=percent
+    let total=((count*percent)/100)+count
+    let inmonth=total/time
+    ay.innerHTML=inmonth
+
+
+}
+
+ 
+
