103dad97a98c281c71e87a451754718f5260210b
ZahraRzazada <zahraer@code.edu.az>
2023-09-19 15:21:01 +0400

    added3

--- js/app.js
+++ js/app.js
@@ -3,9 +3,10 @@ btn.onclick=function(){
     let count =parseInt(document.getElementById('count').value)
     let percent =parseInt(document.getElementById('percent').value)
     let time =parseInt(document.getElementById('time').value)
-    mebleg.innerHTML=count
+   
     faiz.innerHTML=percent
     let total=((count*percent)/100)+count
+    mebleg.innerHTML=total
     let inmonth=total/time
     ay.innerHTML=inmonth
 }
